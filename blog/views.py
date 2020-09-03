23e208a9c53a2e6861fd291dba26b7a80418310b
u101004132 <dzag17188@gmail.com>
2020-09-03 13:34:40 -0400

    posts are now being pulled from db, not using the hard coded dummy posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 posts = [
     {
@@ -25,7 +26,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
