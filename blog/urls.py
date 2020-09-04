c3352c8b79578dd562ad3ea5cd07aade4c55f3cb
u101004132 <dzag17188@gmail.com>
2020-09-04 12:01:44 -0400

    update working

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog_home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('about/', views.about, name='blog_about')
 ]
 
