7fe151af632ef38585439ff3f2b0ebd0f82ccd0d
u101004132 <dzag17188@gmail.com>
2020-09-04 13:14:07 -0400

    new origin

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
-
+FIX BY WATCHING VIDS AGAIN
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
