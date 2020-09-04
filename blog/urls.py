56486af24e47ef554e31f215ea49686556e20353
u101004132 <dzag17188@gmail.com>
2020-09-04 11:22:35 -0400

    last min post work

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog_home'),
+    path('', PostListView.as_view(), name='blog_home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('about/', views.about, name='blog_about')
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+""" from .forms import UserRegisterForm, UserCreationForm, UserUpdateForm """
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
